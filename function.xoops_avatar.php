73c16a994ceaa0f530d48a7720331c01d0f9ad2a
RyujiAMANO <ryuji@ryus.co.jp>
2013-05-30 20:19:27 +0900

    avatarプラグイン追加

---
+++ function.xoops_avatar.php
@@ -0,0 +1,22 @@
+<?php
+
+//js読み込み
+function smarty_function_xoops_avatar($params, &$smarty)
+{
+    $user = $params['user'];
+    $style = isset($params['style']) ? $params['style'] : '';
+
+    if($user->get('user_avatar') != "blank.gif"){
+        $tag = sprintf('<img src="%s/%s" alt="Avatar" title="Avatar" style="%s" />',
+            XOOPS_UPLOAD_URL,
+            $user->getShow('user_avatar'),
+            $style
+        );
+    }else{
+        $tag = sprintf('<img src="%s/modules/user/images/no_avatar.gif" alt="No Avatar" title="No Avatar" style="%s" />',
+            XOOPS_URL,
+            $style
+        );
+    }
+    echo $tag;
+}
